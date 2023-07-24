4d6ad3367ed04d473f51270ae4375135eca15f2d
wacmoua <b.creteur@live.fr>
2023-07-25 00:50:05 +0200

    2eme add

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const scroll = new locomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    tablet: { smooth: true },
+    smatphone: { smooth: true },
+});
